8b0ca5823685d3283fd3b5d59a284f5832a47b92
SonSang <shh1295@gmail.com>
2020-10-27 14:30:21 +0900

    Added revolution surface 3d

---
+++ Surface/RevolutionSurface3d.cpp
@@ -0,0 +1,62 @@
+#include "RevolutionSurface3d.h"
+
+namespace MN {
+	RevolutionSurface3d RevolutionSurface3d::create(const Freeform2dc::Ptr profile) {
+		RevolutionSurface3d surf;
+		surf.profile = profile;
+		return surf;
+	}
+	RevolutionSurface3d::Ptr RevolutionSurface3d::createPtr(const Freeform2dc::Ptr profile) {
+		return std::make_shared<RevolutionSurface3d>(create(profile));
+	}
+
+	static inline Vec3 rotY(const Vec2& v, Real radian) {
+		return { v[0] * cos(radian), v[1], v[0] * sin(radian) };
+	}
+	Vec3 RevolutionSurface3d::evaluate(Real u, Real v) const {
+		auto curvePoint = profile->evaluate(u);
+		return rotY(curvePoint, v);
+	}
+	Vec3 RevolutionSurface3d::differentiate(Real u, Real v, int uOrder, int vOrder) const {
+		if (uOrder == 0 && vOrder == 0)
+			return evaluate(u, v);
+		else if (uOrder == 1 && vOrder == 0) {
+			auto curveDeriv = profile->differentiate(u, 1);
+			return rotY(curveDeriv, v);
+		}
+		else if (uOrder == 0 && vOrder == 1) {
+			auto curvePoint = profile->evaluate(u);
+			return { curvePoint[0] * -sin(v), 0, curvePoint[0] * cos(v) };
+		}
+		else if (uOrder == 2 && vOrder == 0) {
+			auto curveDeriv = profile->differentiate(u, 2);
+			return rotY(curveDeriv, v);
+		}
+		else if (uOrder == 1 && vOrder == 1) {
+			auto curveDeriv = profile->differentiate(u, 1);
+			return { curveDeriv[0] * -sin(v), 0, curveDeriv[0] * cos(v) };
+		}
+		else if (uOrder == 0 && vOrder == 2) {
+			auto curvePoint = profile->evaluate(u);
+			return { curvePoint[0] * -cos(v), 0, curvePoint[0] * -sin(v) };
+		}
+		else if (uOrder == 3 && vOrder == 0) {
+			auto curveDeriv = profile->differentiate(u, 3);
+			return rotY(curveDeriv, v);
+		}
+		else if (uOrder == 2 && vOrder == 1) {
+			auto curveDeriv = profile->differentiate(u, 2);
+			return { curveDeriv[0] * -sin(v), 0, curveDeriv[0] * cos(v) };
+		}
+		else if (uOrder == 1 && vOrder == 2) {
+			auto curveDeriv = profile->differentiate(u, 1);
+			return { curveDeriv[0] * -cos(v), 0, curveDeriv[0] * -sin(v) };
+		}
+		else if (uOrder == 0 && vOrder == 3) {
+			auto curvePoint = profile->evaluate(u);
+			return { curvePoint[0] * sin(v), 0, curvePoint[0] * -cos(v) };
+		}
+		else
+			throw(std::runtime_error("Bezier surface differentiation is only allowed up to 2nd derivatives"));
+	}
+}
---
+++ Surface/RevolutionSurface3d.h
@@ -0,0 +1,41 @@
+/*
+ *******************************************************************************************
+ * Author	: Sang Hyun Son
+ * Email	: shh1295@gmail.com
+ * Github	: github.com/SonSang
+ *******************************************************************************************
+ */
+
+#ifndef __MN_REVOLUTION_SURFACE_3D_H__
+#define __MN_REVOLUTION_SURFACE_3D_H__
+
+#ifdef _MSC_VER
+#pragma once
+#endif
+
+#include "../Freeform.h"
+#include "../Curve/BsplineCurve2d.h"
+#include <memory>
+
+namespace MN {
+	class RevolutionSurface3d : public Freeform3ds {
+	private:
+		RevolutionSurface3d() = default;
+
+		// Profile curve on XY plane that is rotated around Y axis to form surface
+		Freeform2dc::Ptr profile;
+	public:
+		using Ptr = std::shared_ptr<RevolutionSurface3d>;
+
+		// @buildMat : Option for building derivMats in creation time
+		static RevolutionSurface3d create(const Freeform2dc::Ptr profile);
+		static Ptr createPtr(const Freeform2dc::Ptr profile);
+
+		// u : Parameter for profile curve evaluation
+		// v : Parameter for rotation around Y axis
+		virtual Vec3 evaluate(Real u, Real v) const;
+		virtual Vec3 differentiate(Real u, Real v, int uOrder, int vOrder) const;
+	};
+}
+
+#endif
